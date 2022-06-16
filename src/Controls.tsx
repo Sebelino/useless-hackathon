438beeccb36c22ecce7b6c85055afa4ef3e6022f
dmitry.malofeev <dim2k2006@yandex.ru>
2022-06-16 19:14:33 +0200

    Update

--- src/Controls.tsx
+++ src/Controls.tsx
@@ -98,21 +98,10 @@ const Controls = () => {
     <div className="Controls">
       <div className="Controls__section">
         <RandomValueControl title="Mode" min={1} max={5} onSubmit={console.log} />
-
-        <div className="Controls__title">Mode</div>
-
-        <RadioButton name="mode" label="1" value="1" />
-        <RadioButton name="mode" label="2" value="2" />
-        <RadioButton name="mode" label="3" value="3" />
-        <RadioButton name="mode" label="4" value="4" />
       </div>
 
       <div className="Controls__section">
-        <div className="Controls__title">Speed</div>
-
-        <RadioButton name="mode" label="1 km/h" value="1" />
-        <RadioButton name="mode" label="1 000 km/h" value="2" />
-        <RadioButton name="mode" label="1 000 000 km/h" value="3" />
+        <RandomValueControl title="Speed" min={1} max={1000} onSubmit={console.log} isSubmitActive={false} />
       </div>
     </div>
   );
